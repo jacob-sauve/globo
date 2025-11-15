917c81a105ff6f6f904466d0cf3025d3ae73b454
jacob-sauve <woozy-army-trivial@duck.com>
2025-11-14 22:34:59 -0500

    Initial failed test with FlightRadar24

--- backend/flight_finder.py
+++ backend/flight_finder.py
@@ -1,7 +1,23 @@
 from FlightRadar24 import FlightRadar24API
+import datetime
+from dotenv import load_dotenv
+import os
+
+# get login
+load_dotenv()
+username = os.getenv("FR24_USERNAME")
+password = os.getenv("FR24_PASSWORD")
 
 # object to start fetching data
 fr_api = FlightRadar24API()
+if not fr_api.is_logged_in():
+    fr_api.login(username, password)
+config = fr_api.get_flight_tracker_config()
+config.limit = "10"
 
-flights = fr_api.get_flights()
-print(flights)
+if __name__ == "__main__":
+    flights = fr_api.get_flights()
+    print(fr_api.get_history_data(flights[0],"CSV", datetime.datetime.now()))
+    # flight_time = datetime.datetime.fromtimestamp(flights.get_flight("").time.scheduled.departure)
+    # print(flight_time)
+
